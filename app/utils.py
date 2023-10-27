b12d5d44ad9453fe718b5e95fd9fa7f919105237
zbuh <nuno@hubz.net>
2023-10-27 19:23:34 +0100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -33,7 +33,7 @@ def send_email(persons: List[Tuple[Person, Person]], title: str, budget: str) ->
 
     mail_status = True
     for friends in persons:
-        content = f"Your secret friend is {friends[1].name}.\n\nPlease buy him a nice gift with {budget}\n"
+        content = f"{friends[1].name} o teu amigo secreto é {friends[1].name}.\n\nCompra-lhe uma prenda porreira no valor de {budget}\n"
         status = service.sendmail(from_addr=sender_mail, to_addrs=friends[0].email, msg=f"Subject: {title}\n{content}")
         if len(status) > 0:
           mail_status = False
